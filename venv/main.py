9034ce76223831393191d969a79a98d466fbaa32
David Schmidt <david.reed.schmidt@gmail.com>
2015-02-01 22:00:07 -0800

    Commit

--- venv/main.py
+++ venv/main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -17,12 +20,11 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-	return '<h1>Hello World!</h1>'
+	return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
 	return render_template('user.html', name=name)
-
 
 if __name__ == '__main__':
 	app.run(debug=True)
